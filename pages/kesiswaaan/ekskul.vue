2b277d43cf1ecb8ff349748b650fe8b174731530
Aisiti03 <143867535+Aisiti03@users.noreply.github.com>
2024-12-28 09:23:07 +0700

    Add files via upload

---
+++ pages/kesiswaaan/ekskul.vue
@@ -0,0 +1,182 @@
+<template>
+    <div class="container">
+        <h4 class="mt-5">Ekstrakurrikuler <br> <span class="text-primary">Siswa</span></h4>
+        <div class="row mt-5 justify-content-center">
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/paskibra.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>Paskibra</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/pramuka.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>Pramuka</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/pmr.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>PMR</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4 justify-content-center">
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/pks.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>PKS</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/plh.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>PLH</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/osis.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>Perisai Diri</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4 justify-content-center">
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/itclub.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>IT Club</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/englishclub.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>English Club</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/cinematography.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>Cinematography</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4 justify-content-center">
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/laskarpustaka.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>Laskar Pustaka</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/irma.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>IRMA</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/osis.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>Seni</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4 justify-content-center">
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/futsal.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>Futsal</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/osis.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>Sepak Bola</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/basket.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>Basket</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5 justify-content-center">
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/osis.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>Voli</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card rounded-2">
+                    <img src="~/assets/img/logo/bulutangkis.png" class="card-img-top rounded-2 mt-3" alt="img-artikel">
+                    <div class="card-body text-center p-2 mb-3 mt-3">
+                        <h6>Bulu Tangkis</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+h4, h6, .btn {
+    font-family: "Poppins", serif;
+}
+
+h4 {
+    margin-left: 95px;
+}
+
+.card {
+    background-color: transparent;
+    backdrop-filter: blur(20px);
+    border: 2px solid #ffff;
+}
+
+.btn {
+    font-size: 12px;
+    width: 150px;
+    border: 2px solid #ffff;
+
+}
+
+.card-img-top {
+    width: 150px;
+    margin: auto;
+}
+
+</style>
